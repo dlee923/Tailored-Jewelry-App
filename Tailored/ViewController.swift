39bf651675ef18b1677355550981df45e57a269c
Daniel Lee <DLee@daniels-mbp.home>
2016-08-02 23:33:17 -0400

    Initial Commit

---
+++ Tailored/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tailored
+//
+//  Created by Daniel Lee on 8/2/16.
+//  Copyright © 2016 DLEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
